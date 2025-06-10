7ac6608c6bd8aa5fc3e8c3b984cb161337e33429
JCassimiro2 <jp380015@gmail.com>
2025-06-09 22:40:57 -0300

    teste3

---
+++ rosto.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Serviços de Rosto – BarberSabará</title>
+  <link rel="stylesheet" href="css/styleRosto.css" />
+</head>
+<body>
+  <header class="header-cabelo">
+    <a href="serviços.php" class="voltar-btn">Voltar</a>
+    <h1>Serviços de Rosto</h1>
+  </header>
+
+  <main class="lista-servicos">
+    <div class="servico">
+      <span>Limpeza de Pele</span>
+      <span>R$ 50</span>
+    </div>
+    <div class="servico">
+      <span>Design de Sobrancelha</span>
+      <span>R$ 30</span>
+    </div>
+    <div class="servico">
+      <span>Máscara Facial</span>
+      <span>R$ 25</span>
+    </div>
+    <div class="servico">
+      <span>Esfoliação</span>
+      <span>R$ 35</span>
+    </div>
+    <div class="servico">
+      <span>Barba Terapêutica</span>
+      <span>R$ 40</span>
+    </div>
+  </main>
+</body>
+</html>
